ecc1c6d38a9afeda28b14208a4be58a5abf2c0b4
Sam Freitas <60946332+Sam-Freitas@users.noreply.github.com>
2021-12-13 16:13:49 -0700

    code update

--- use_network_dynamic.py
+++ use_network_dynamic.py
@@ -1,27 +1,9 @@
-#Courtney Comrie and Sam Freitas
-#ECE 523 Final Project
-#UNET-CNN for segmenting brains from the skull
-
 import numpy as np #import needed libraries and commands
 import pandas as pd 
 import matplotlib.pyplot as plt
-import cv2
-from cv2 import imread
 import os
-import sys
-from tqdm import tqdm
-import random
-import warnings
-from itertools import chain
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import Input, Dropout, Lambda, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras import backend as K
 import tensorflow as tf
 import shutil
-import glob
-from natsort import natsorted
 
 from SA_dynamic_unet import dynamic_unet_cnn, plot_figures, data_generator_for_testing,load_first_image_get_size,get_num_layers_unet
 
@@ -29,18 +11,8 @@ plt.ion() #turn ploting on
 
 dataset_path = os.getcwd()
 image_path = os.path.join(dataset_path, "testing")
-# dataset = pd.read_csv('dataset.csv')
-
-# total = len(dataset) #set variables
-# test_split = 0.2
-# height = 128
-# width = 128
 channels = 1 
 batch_size = 1
-
-## 128 - 2
-## 512 - 4
-## 1024 - 6 ???????
 
 img_size = load_first_image_get_size(image_path)
 num_layers_of_unet, img_size = get_num_layers_unet(img_size)
